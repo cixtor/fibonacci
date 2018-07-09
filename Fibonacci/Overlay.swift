323092a72aa8d2e3f2ef9e497528dfcdfa8c634e
Cixtor <cixtords@gmail.com>
2018-07-09 09:00:00 -0700

    Add game view overlay to paint the swipeable surface

---
+++ Fibonacci/Overlay.swift
@@ -0,0 +1,15 @@
+//
+//  Overlay.swift
+//  Fibonacci
+//
+//  Created by Yorman on 7/1/18.
+//  Copyright © 2018 yorman. All rights reserved.
+//
+
+import UIKit
+
+class Overlay: UIView {
+    @IBOutlet weak var message: UILabel!
+    @IBOutlet weak var keepPlaying: UIButton!
+    @IBOutlet weak var restartGame: UIButton!
+}
